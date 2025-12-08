36b7ad16859b4fb8dd83e2830e6512bd3ca195b4
Ana Carolina Albuquerque <anacarolina.albuquerque.v@gmail.com>
2025-12-08 13:33:53 -0300

    Colocando as imagens na pasta public- Part1

--- src/constants/goalIcons.js
+++ src/constants/goalIcons.js
@@ -1,199 +1,78 @@
- import animal from "../assets/icons/animal.png";
-import bandaid from "../assets/icons/bandaid.png";
-import bike from "../assets/icons/bike.png";
-import blacklivesmatter from "../assets/icons/blacklivesmatter.png";
-import books from "../assets/icons/books.png";
-import bride from "../assets/icons/bride.png";
-import briefcase from "../assets/icons/briefcase.png";
-import business from "../assets/icons/business.png";
-import cake from "../assets/icons/cake.png";
-import car from "../assets/icons/car.png";
-import card from "../assets/icons/card.png";
-import cart from "../assets/icons/cart.png";
-import church from "../assets/icons/church.png";
-import clothes from "../assets/icons/clothes.png";
-import coin from "../assets/icons/coin.png";
-import compass from "../assets/icons/compass.png";
-import computer from "../assets/icons/computer.png";
-import cone from "../assets/icons/cone.png";
-import costume from "../assets/icons/costume.png";
-import croche from "../assets/icons/croche.png";
-import cruise from "../assets/icons/cruise.png";
-import dog from "../assets/icons/dog.png";
-import donation from "../assets/icons/donation.png";
-import dress from "../assets/icons/dress.png";
-import driverslicense from "../assets/icons/driverslicense.png";
-import education from "../assets/icons/education.png";
-import gamecontroller from "../assets/icons/gamecontroller.png";
-import gift from "../assets/icons/gift.png";
-import guitar from "../assets/icons/guitar.png";
-import gym from "../assets/icons/gym.png";
-import habbit from "../assets/icons/habbit.png";
-import hammer from "../assets/icons/hammer.png";
-import headset from "../assets/icons/headset.png";
-import health from "../assets/icons/health.png";
-import heart from "../assets/icons/heart.png";
-import house from "../assets/icons/house.png";
-import ideia from "../assets/icons/ideia.png";
-import label from "../assets/icons/label.png";
-import lgbt from "../assets/icons/lgbt.png";
-import makeup from "../assets/icons/makeup.png";
-import medicine from "../assets/icons/medicine.png";
-import mike from "../assets/icons/mike.png";
-import money from "../assets/icons/money.png";
-import moneybag from "../assets/icons/moneybag.png";
-import motorcycle from "../assets/icons/motorcycle.png";
-import mountain from "../assets/icons/mountain.png";
-import needle from "../assets/icons/needle.png";
-import oldman from "../assets/icons/oldman.png";
-import oldwoman from "../assets/icons/oldwoman.png";
-import paint from "../assets/icons/paint.png";
-import painting from "../assets/icons/painting.png";
-import passport from "../assets/icons/passport.png";
-import phone from "../assets/icons/phone.png";
-import piano from "../assets/icons/piano.png";
-import piggy from "../assets/icons/piggy.png";
-import plane from "../assets/icons/plane.png";
-import plant from "../assets/icons/plant.png";
-import retirement from "../assets/icons/retirement.png";
-import returnmoney from "../assets/icons/returnmoney.png";
-import ring from "../assets/icons/ring.png";
-import selfcare from "../assets/icons/selfcare.png";
-import show from "../assets/icons/show.png";
-import skate from "../assets/icons/skate.png";
-import smile from "../assets/icons/smile.png";
-import solarenergy from "../assets/icons/solarenergy.png";
-import tenis from "../assets/icons/tenis.png";
-import theater from "../assets/icons/theater.png";
-import theater2 from "../assets/icons/theater2.png";
-import ceramic from "../assets/icons/ceramic.png";
-import trans from "../assets/icons/trans.png";
-import trunk from "../assets/icons/trunk.png";
-import vacation from "../assets/icons/vacation.png";
-import wallet from "../assets/icons/wallet.png";
-import cat from "../assets/icons/cat.png";
-import food from "../assets/icons/food.png";
-import tattoo from "../assets/icons/tattoo.png";
-import teddybear from "../assets/icons/teddybear.png";
-import ticket from "../assets/icons/ticket.png";
-import baby1 from "../assets/icons/baby1.png";
-import baby2 from "../assets/icons/baby2.png";
-import kid1 from "../assets/icons/kid1.png";
-import kid2 from "../assets/icons/kid2.png";
-import kid3 from "../assets/icons/kid3.png";
-import kid4 from "../assets/icons/kid4.png";
-import kid5 from "../assets/icons/kid5.png";
-import kid6 from "../assets/icons/kid6.png";
-import kid7 from "../assets/icons/kid7.png";
-import kid8 from "../assets/icons/kid8.png"; 
-import kid9 from "../assets/icons/kid9.png"; 
-import kid10 from "../assets/icons/kid10.png"; 
-import adult1 from "../assets/icons/adult1.png";  
-import adult2 from "../assets/icons/adult2.png";  
-import adult3 from "../assets/icons/adult3.png"; 
-import adult4 from "../assets/icons/adult4.png"; 
-import adult5 from "../assets/icons/adult5.png"; 
-import adult6 from "../assets/icons/adult6.png"; 
-import adult7 from "../assets/icons/adult7.png";  
-import adult8 from "../assets/icons/adult8.png";  
-import oldman1 from "../assets/icons/oldman1.png";  
-import oldman2 from "../assets/icons/oldman2.png";  
-import humanrights from "../assets/icons/humanrights.png";  
-import autism from "../assets/icons/autism.png";  
-import neurodivergent from "../assets/icons/neurodivergent.png";  
-import policy from "../assets/icons/policy.png";  
-import map from "../assets/icons/map.png";  
-import drink from "../assets/icons/drink.png";  
-import christmas from "../assets/icons/christmas.png"; 
-import newyear from "../assets/icons/newyear.png"; 
-import yo15 from "../assets/icons/15yo.png"; 
-import eletricguitar from "../assets/icons/eletricguitar.png"; 
-import fun from "../assets/icons/fun.png";
-import party from "../assets/icons/party.png"; 
-import spider from "../assets/icons/spider.png";
-import soccerball from "../assets/icons/soccerball.png";
-import flowers from "../assets/icons/flowers.png";
-import singlemoney from "../assets/icons/singlemoney.png";
-import kid from "../assets/icons/kid.png";
-import friends from "../assets/icons/friends.png";
-import activism from "../assets/icons/activism.png";
-import cinema from "../assets/icons/cinema.png";
- 
-// export const availableIcons = [ /* ... */ ];
+// ATENÇÃO: Nenhum import JS é mais necessário, pois estamos referenciando as imagens diretamente pelo caminho estático.
 
 export const availableIcons = [ 
     {
         title: "icon_categories.finance",
         icons: [
-            coin, money, moneybag,
-            wallet,returnmoney, card, trunk, cart, label, ticket, piggy, smile,
+            "/icons/coin.png", "/icons/money.png", "/icons/moneybag.png",
+            "/icons/wallet.png", "/icons/returnmoney.png", "/icons/card.png", "/icons/trunk.png", "/icons/cart.png", "/icons/label.png", "/icons/ticket.png", "/icons/piggy.png", "/icons/smile.png",
         ]
     }, 
     {
         title: "icon_categories.health",
         icons: [
-            health, heart, bandaid, medicine, needle, gym, selfcare,
-            makeup, 
+            "/icons/health.png", "/icons/heart.png", "/icons/bandaid.png", "/icons/medicine.png", "/icons/needle.png", "/icons/gym.png", "/icons/selfcare.png",
+            "/icons/makeup.png", 
         ]
     }, 
     {
         title: "icon_categories.home_work",
         icons: [
-            house, retirement, hammer, cone, solarenergy, plant, business, briefcase, 
+            "/icons/house.png", "/icons/retirement.png", "/icons/hammer.png", "/icons/cone.png", "/icons/solarenergy.png", "/icons/plant.png", "/icons/business.png", "/icons/briefcase.png", 
         ]
-    },     
+    },     
     {
         title: "icon_categories.travel_transport_docs",
         icons: [
-            driverslicense, passport, mountain, vacation,
-            skate, bike, motorcycle, car, plane, cruise, compass, map
+            "/icons/driverslicense.png", "/icons/passport.png", "/icons/mountain.png", "/icons/vacation.png",
+            "/icons/skate.png", "/icons/bike.png", "/icons/motorcycle.png", "/icons/car.png", "/icons/plane.png", "/icons/cruise.png", "/icons/compass.png", "/icons/map.png"
         ]
     }, 
     {
         title: "icon_categories.tech_education",
         icons: [
-            education, books, ideia, computer,
-            phone, headset, gamecontroller, mike, 
+            "/icons/education.png", "/icons/books.png", "/icons/ideia.png", "/icons/computer.png",
+            "/icons/phone.png", "/icons/headset.png", "/icons/gamecontroller.png", "/icons/mike.png", 
         ]
     }, 
     {
         title: "icon_categories.shopping",
         icons: [ 
-            clothes, costume, dress, tenis, tattoo, teddybear, food, drink,
+            "/icons/clothes.png", "/icons/costume.png", "/icons/dress.png", "/icons/tenis.png", "/icons/tattoo.png", "/icons/teddybear.png", "/icons/food.png", "/icons/drink.png",
         ]
     }, 
     {
         title: "icon_categories.relationships_events",
         icons: [
-            ring, bride, church, newyear, yo15, cake,
-            gift, christmas,  
+            "/icons/ring.png", "/icons/bride.png", "/icons/church.png", "/icons/newyear.png", "/icons/15yo.png", "/icons/cake.png",
+            "/icons/gift.png", "/icons/christmas.png",   
         ]
     }, 
     {
         title: "icon_categories.art_culture",
         icons: [
-            guitar, eletricguitar, piano, show, croche, paint,
-            painting, ceramic,   
-            fun, cinema, theater, theater2,  
+            "/icons/guitar.png", "/icons/eletricguitar.png", "/icons/piano.png", "/icons/show.png", "/icons/croche.png", "/icons/paint.png",
+            "/icons/painting.png", "/icons/ceramic.png",   
+            "/icons/fun.png", "/icons/cinema.png", "/icons/theater.png", "/icons/theater2.png",   
         ]
     }, 
     {
         title: "icon_categories.social_causes",
         icons: [
-            blacklivesmatter, lgbt, trans, donation, humanrights, autism, neurodivergent, policy,
+            "/icons/blacklivesmatter.png", "/icons/lgbt.png", "/icons/trans.png", "/icons/donation.png", "/icons/humanrights.png", "/icons/autism.png", "/icons/neurodivergent.png", "/icons/policy.png",
         ]
     }, 
     {
         title: "icon_categories.pets",
         icons: [ 
-            animal, dog, cat, habbit,
+            "/icons/animal.png", "/icons/dog.png", "/icons/cat.png", "/icons/habbit.png",
         ]
     }, 
     {
         title: "icon_categories.people",
         icons: [ 
-            baby1, baby2, kid1, kid2, kid10, kid5, kid3, kid4, kid6, kid7, kid8, kid9, adult1, adult2, adult3, adult4, adult5, adult6, adult7, adult8,
-            oldman, oldman1, oldman2, oldwoman,
+            "/icons/baby1.png", "/icons/baby2.png", "/icons/kid1.png", "/icons/kid2.png", "/icons/kid10.png", "/icons/kid5.png", "/icons/kid3.png", "/icons/kid4.png", "/icons/kid6.png", "/icons/kid7.png", "/icons/kid8.png", "/icons/kid9.png", "/icons/adult1.png", "/icons/adult2.png", "/icons/adult3.png", "/icons/adult4.png", "/icons/adult5.png", "/icons/adult6.png", "/icons/adult7.png", "/icons/adult8.png",
+            "/icons/oldman.png", "/icons/oldman1.png", "/icons/oldman2.png", "/icons/oldwoman.png",
         ]
     },
 ];
